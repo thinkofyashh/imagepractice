427c2b8c37bc98a051124cd0ac0fcd9c43a8563d
yashrawat <yashrawat682@gmail.com>
2023-04-29 10:10:03 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/imagepractice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.imagepractice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        actions: [Icon(Icons.email)],
+        elevation: 30,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: [
+                Expanded(
+                    child: Container(
+                  height: 300,
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+                  margin: EdgeInsets.all(5),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.red),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          spreadRadius: 5,
+                          blurRadius: 1,
+                        )
+                      ],
+                      image: DecorationImage(
+                        opacity: 0.25,
+                        image: AssetImage('assets/images/sunflower.jpeg'),
+                      )),
+                  child: Text(
+                    'assets/images/sunflower.jpeg/assets/images/sunflower.jpeg',
+                    textAlign: TextAlign.center,
+                    maxLines: 30,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                )),
+                SizedBox(width: 20,),
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
